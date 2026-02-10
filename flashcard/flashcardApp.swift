cd817827705d3f5024acc1d24aa12ddc4514ea2d
faiyamm <sofia.almaraz@uabc.edu.mx>
2026-02-09 16:59:11 -0800

    Initial Commit

---
+++ flashcard/flashcardApp.swift
@@ -0,0 +1,17 @@
+//
+//  flashcardApp.swift
+//  flashcard
+//
+//  Created by Fai on 09/02/26.
+//
+
+import SwiftUI
+
+@main
+struct flashcardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
